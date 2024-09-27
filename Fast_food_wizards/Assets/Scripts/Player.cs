3fbb1e78180ac69a12e44c68700ee09d470863e6
hotbloxer <33450949+hotbloxer@users.noreply.github.com>
2024-09-27 14:07:59 +0200

    initial player setup

--- Fast_food_wizards/Assets/Scripts/Player.cs
+++ Fast_food_wizards/Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float horizontal;
+    private float speed;
+    private float jumping_power = 16f;
+    private bool is_facign_right = true;
+
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform ground_check;
+    [SerializeField] private LayerMask ground_layer;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2 (horizontal * speed, 0);
     }
 }
